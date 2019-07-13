44ce7410a137ffb123263415783b9e760e9e3c34
Mike Kang <mike@Mikes-MacBook-Pro.local>
2019-07-13 23:04:16 +1000

    chat layout

---
+++ resources/js/components/Chat.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex h-full">
+        <div class="w-1/5 border-r-2 border-solid border-gray-600">User list</div>
+        <div class="w-4/5 flex flex-col">
+            <div class="flex-1">Chat Area</div>
+            <div class="flex-initial p-2">
+                <input class="border-2 border-solid rounded border-gray-600 w-full p-3" type="text">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style>
+
+</style>
